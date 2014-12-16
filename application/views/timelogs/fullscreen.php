9a36f0405d93b797eacf570107d530460b0750be
Mike Dombroski <mike@tarpon.(none)>
2014-12-15 20:38:12 -0500

    Close button base URL updated

--- application/views/timelogs/fullscreen.php
+++ application/views/timelogs/fullscreen.php
@@ -1,11 +1,10 @@
-<a href="/rab/events" class="close_button">X</a>
+<a href="<?php echo URL; ?>events" class="close_button">X</a>
 
 <div class="centered">
   Welcome! Please sign in:
-  
   <br>
   <br>
-  <div class="label_div">Enter your last name : </div>
+  <div class="label_div">Enter your last name:</div>
   <div class="input_container">
       <input type="text" id="autofill" onkeyup="autofill(<?php echo $event;?>)"/>
       <ul id="autofill_results" style="display: none; width: 500px; margin: 50px auto;">
